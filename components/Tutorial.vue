1606c07fd54416fd9c80ba222c8c3f6410ce62db
ming <reedbf@126.com>
2022-06-06 15:40:36 +0800

    A

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -119,24 +119,27 @@
     <div class="flex">
       <div class="test">
         <BarChart
-          key="chartData1"
+          :key="chartData1"
           :chartData="chartData1"
           :refName="'chart1'"
         ></BarChart>
       </div>
       <div class="test">
         <BarChart
-          key="chartData2"
+          :key="chartData2"
           :chartData="chartData2"
           :refName="'chart2'"
         ></BarChart>
       </div>
+      <div class="test">
+        <LineChart
+        :key="chartData2"
+        :chartData="chartData2"
+        :refName="'chart3'"
+      ></LineChart>
+      </div>
     </div>
-    <!-- <LineChart
-        :key="chartData1 + 1"
-        :chartData="chartData1"
-        :refName="'chart1'"
-      ></LineChart> -->
+   
   </div>
 </template>
 
