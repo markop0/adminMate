c26c3f77341268964845a18e96552ceaf110b80f
ming <reedbf@126.com>
2022-06-09 17:14:19 +0800

    A

--- main.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-// 自定义插件引入
-import BarChart from './lib'
-import LineChart from './lib'
-// 使用插件
-Vue.use(BarChart, LineChart);
--- plugins/echarts.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-import echarts from 'echarts'
-Vue.prototype.$echarts = echarts // 引入组件（全局注册）
